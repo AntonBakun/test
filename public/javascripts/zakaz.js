ea7f9eefcf8c2f8e255548f863ac0eb97bccd1c0
AntonBakun <antonbakunworkacc@gmail.com>
2016-04-20 20:03:06 +0300

    node сервер+express

---
+++ public/javascripts/zakaz.js
@@ -0,0 +1,15 @@
+/**
+ * Created by BAKUN on 29.02.2016.
+ */
+
+var app = angular.module('myApp', []);
+app.controller('myOrder', function ($scope) {
+	$scope.name = "";
+	$scope.phone = "";
+	$scope.to = "";
+	$scope.from = "";
+	$scope.car = "";
+	$scope.bus = "";
+	$scope.vechile "";
+	$scope.discription = "";
+});
