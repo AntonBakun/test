8f65f4d2b66d323a9fb634a93cc32f1edff3945d
AntonBakun <antonbakunworkacc@gmail.com>
2016-04-25 22:34:44 +0300

    подключение к mongolab.com ,облачная бд .

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+
+
+/* DB connection */
+mongoose.connect('mongodb://<taxiserver>:<servertaxi>@ds019491.mlab.com:19491/db_service');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function  (){
+	console.log("succesful database connection");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
