b6af82b8d6b5f25f1b71ab71680fbbc7d99b1bcc
AntonBakun <antonbakunworkacc@gmail.com>
2016-05-26 00:08:31 +0300

    post and admin dashboard

--- app/index.js
+++ app/index.js
@@ -4,38 +4,78 @@ var mongoose = require('mongoose');
 var database = require('./config/database');
 mongoose.connect(database.url);
 var db = mongoose.connection;
+//var app = require('./')
 db.on('error', console.error.bind(console, 'connection error:'));
 
 db.once('open', function callback (){
     console.log("successful database connection");
 });
-var newSave = require('./databasefunc');
-function debuggg ()
-{
-    for(  var i = 1 ; i<3 ; i++ )
-    {
-         return console.log(newSave.newRec.i);
-    }
-}
- debuggg();
+var ord =require('./datamodel');
+//require('./datamodel'); ///new collection orders model
 
-//var SaveNewSave  = function (){
-  //  for (var i = 1; i < 3; i++) {
-  //      newSave.newRec.i.save(function (err) {
-  //          if (err) {
-  //              console.log(err);
-  //          } else {
-  //              console.log('save good rec');
-  //              console.log( newSave.newRec.i);
-  //          }
-  //      });
-  //  }
-//};
-//SaveNewSave();
+var newOrders = new ord.orderline ({
+    AdressFrom:'советская 44',
+    AdressTo:'суворова 96к3',
+    Phone:'336739460',
+    Name: 'jon doe',
+    Vechile:'легковая',
+    Discription:'бар матрикс',
+    OrderStatus:'wait',
+    DateTime: new Date().toLocaleString()
+});
+
+
+var newOrdersDisp = new ord.dispatcher ({
+    AdressFrom:'советская 44',
+    AdressTo:'суворова 96к3',
+    Phone:'336739460',
+    Name: 'jon doe',
+    Vechile:'легковая',
+    Discription:'бар матрикс',
+    OrderStatus:'wait',
+    DateTime: new Date().toLocaleString()
+});
+
+//newOrders.save(function (err) {
+         // if (err) {console.log(err);
+         //} else {
+            // console.log('save good rec');
+            //     console.log(newOrders );
+            //}
+       // });
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/taxi', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/taxi.admin', function(req, res, next) {
+    res.render('admin', { title: 'Express' });
+});
+
+router.post('/orderform',function(req,res){
+    console.log(req.body);
+    var newOrders = new ord.orderline ({
+        AdressFrom: req.body.adrfrom,
+        AdressTo:req.body.adrto,
+        Phone: req.body.phone,
+        Name: req.body.username,
+        Vechile:req.body.vehicle,
+        Discription:req.body.adrdescription,
+        OrderStatus:req.body.orderstatusadr,
+        DateTime: new Date().toLocaleString()
+    });
+    newOrders.save(function (err) {
+         if (err) {console.log(err);
+        } else {
+         console.log('save good rec');
+             console.log(newOrders );
+        }
+    });
+
+
+    res.end("success");
 });
 
 module.exports = router;
--- public/javascripts/zakaz.js
+++ public/javascripts/zakaz.js
@@ -3,13 +3,56 @@
  */
 
 var app = angular.module('myApp', []);
-app.controller('myOrder', function ($scope) {
-	$scope.name = "";
-	$scope.phone = "";
-	$scope.to = "";
-	$scope.from = "";
-	$scope.car = "";
-	$scope.bus = "";
-	$scope.vechile="";
-	$scope.discription = "";
+app.controller('myOrder', function ($scope,$http) {
+
+	$scope.newOrder = {orderstatusadr:'0'
+		};
+
+	$scope.update = function(user) {
+		$scope.newOrder = angular.copy(user);
+		//console.log($scope.newOrder);
+		var data = $scope.newOrder;
+		$http.post('/orderform', data)
+				.success(function (data, status, headers, config) {
+					$scope.PostDataResponse = data;
+					console.log($scope.PostDataResponse);
+				})
+				.error(function (data, status, headers, config) {
+					$scope.ResponseDetails = {
+						Data: data,
+						status: status,
+						header: headers,
+						config: config
+					};
+					console.log($scope.ResponseDetails);
+				});
+
+
+	};
+
+
+
+	$scope.reset = function() {
+		$scope.user = angular.copy($scope.newOrder);
+
+	};
+
+	$scope.reset();
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
